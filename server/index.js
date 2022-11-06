27c6057bf918c6eaaa9be4cb56ec5abc85a253da
CanvasCao <1174478646@aqq.com>
2022-11-06 23:24:27 +1100

    Test git email

--- server/index.js
+++ server/index.js
@@ -6,7 +6,6 @@ const server = require('http').createServer(app);
 const io = require('socket.io')(server);
 const port = process.env.PORT || 3000;
 
-
 server.listen(port, () => {
   console.log('Server listening at port %d', port);
 });
