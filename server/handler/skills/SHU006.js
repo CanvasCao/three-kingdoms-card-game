7d5c6850afcdb4e8feb8d781ea700a2266bcd400
CanvasCao <1174478646@qq.com>
2023-07-30 21:37:37 +1000

    refactor

--- server/handler/skills/SHU006.js
+++ server/handler/skills/SHU006.js
@@ -1,14 +1,15 @@
 const pandingEvent = require("../../event/pandingEvent");
 const {findOnGoingUseStrikeEventSkill} = require("../../event/utils");
 const handleShu006TieJiResponse = (gameStatus, response) => {
-    const skillResponse = gameStatus.skillResponse;
-    const skillNameKey = gameStatus.skillResponse.skillNameKey;
     const chooseToReleaseSkill = response.chooseToResponse;
 
     const onGoingUseStrikeEventSkill = findOnGoingUseStrikeEventSkill(gameStatus);
     onGoingUseStrikeEventSkill.done = true;
 
     if (chooseToReleaseSkill) {
+        const skillNameKey = gameStatus.skillResponse?.skillNameKey;
+        const skillResponse = gameStatus.skillResponse;
+
         pandingEvent.generatePandingEventThenSetNextPandingEventSkillToSkillResponse(gameStatus, {
             originId: skillResponse.playerId,
             pandingNameKey: skillNameKey
