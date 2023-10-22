44dfae36ad3b7835322ca4d41bf96e5a531f0d6b
CanvasCao <1174478646@qq.com>
2023-10-22 14:09:24 +1100

    [SGS-214] Add ZHEN_JI

--- server/model/GameEngine.js
+++ server/model/GameEngine.js
@@ -108,7 +108,7 @@ class GameEngine {
 
             // 选将
             const allSelectHeroIds = [
-                "WEI001", "WEI002", "WEI003", "WEI004", 'WEI005',
+                "WEI001", "WEI002", "WEI003", "WEI004", 'WEI005', "WEI007",
                 "SHU001", "SHU002", "SHU003", "SHU005", "SHU006", "SHU007",
                 "WU001", "WU002", "WU003", "WU004", "WU006",
                 "QUN002"];
@@ -116,7 +116,6 @@ class GameEngine {
             const canSelectHeroNumber = process.env.NODE_ENV == 'production' ? 3 : allSelectHeroIds.length
             const canSelectHeroIds = [
                 ...sampleSize(allSelectHeroIds, canSelectHeroNumber),
-                "WEI007"
             ]
             // "SP001"];
             newPlayer.canSelectHeros = canSelectHeroIds.map(heroId => getHeroConfig(heroId))
